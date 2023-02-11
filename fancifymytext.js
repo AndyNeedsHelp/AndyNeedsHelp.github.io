6200826aa16c728cfbe0ef2a92241acf500e168b
AndyNeedsHelp <andrewzavala30@gmail.com>
2023-02-10 16:54:31 -0800

    im upset

--- fancifymytext.js
+++ fancifymytext.js
@@ -10,5 +10,5 @@ function ChangeSize()
 
 function BackSize()
 {
-    document.getElementById("EnterThing").style.fontSize= "4px";
+    document.getElementById("EnterThing").style.fontSize= "6px";
 }
