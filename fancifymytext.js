34c800c914c82e77e7195d5f6d6ddd3a0f0b0848
AndyNeedsHelp <andrewzavala30@gmail.com>
2023-02-19 17:27:39 -0800

    mmooomooo

--- fancifymytext.js
+++ fancifymytext.js
@@ -11,9 +11,22 @@ function ChangeSize()
 function FancyFancy()
 {
     document.getElementById("EnterThing").style.fontWeight = 'bold';
+    document.getElementById("EnterThing").style.color = "blue";
+    document.getElementById("EnterThing").style.textDecoration = "underline";
 }
 
 function BoringBoring()
 {
     document.getElementById("EnterThing").style.fontWeight = '400';
+    document.getElementById("EnterThing").style.color = "black";
+    document.getElementById("EnterThing").style.textDecoration = "none";
+}
+
+function Moo()
+{
+    document.getElementById("EnterThing").style.textTransform = "uppercase";
+    var userInput = document.getElementById("EnterThing").value;
+    userInput = userInput.split(".");
+    userInput - userInput.join("-moo");
+    document.getElementById("EnterThing").value = userInput;
 }
